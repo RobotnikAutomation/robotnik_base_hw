33e933782cf8226725280438d38e7175754675d4
asoriano1 <asoriano@robotnik.es>
2017-08-04 09:59:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# robotnik_base_hw
+
+ROS controller component based on ros_control architecture. 
+
+This component is compatible with most of the Robotnik's motor hardware.
+
+## Dependencies
+
+This component depends on the library robotnik_base_hw_lib
+
+
+
+
---
+++ src/robotnik_base_hw_node.cpp
@@ -0,0 +1,80 @@
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+
+#include <vector>
+
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>               // battery, gyro, and steering angle
+#include <std_msgs/Float64.h>               // battery, gyro, and steering angle
+#include <std_msgs/Int32.h>               
+#include <std_msgs/Bool.h>                  // 
+#include <sensor_msgs/JointState.h>
+
+#include <robotnik_msgs/MotorStatus.h>
+
+#include <self_test/self_test.h>
+#include <diagnostic_msgs/DiagnosticStatus.h>
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <diagnostic_updater/update_functions.h>
+#include <diagnostic_updater/DiagnosticStatusWrapper.h>
+#include <diagnostic_updater/publisher.h>
+
+#include <robotnik_base_hw_lib/robotnik_base_hw.h>
+
+
+
+// MAIN
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "robotnik_base_hw_node");
+    
+    // TODO: remove debug level
+    if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) { 
+        ros::console::notifyLoggerLevelsChanged();
+    } 
+
+    ros::NodeHandle nh("");
+    // NOTE: We run the ROS loop in a separate thread as external calls such
+    // as service callbacks to load controllers can block the (main) control loop
+    ros::AsyncSpinner spinner(2);
+    spinner.start();
+
+    // Create the hardware interface specific to your robot
+    boost::shared_ptr<RobotnikBaseHW> robotnik_base_hw_lib (new RobotnikBaseHW(nh));   
+    robotnik_base_hw_lib->initHardwareInterface();
+    robotnik_base_hw_lib->Setup();
+    robotnik_base_hw_lib->Start();
+
+    //sleep(5);
+    //robotnik_base_hw_lib->home();
+
+    robotnik_base_hw_lib->waitToBeReady();
+
+    // Start the control loop
+    controller_manager::ControllerManager cm(&(*robotnik_base_hw_lib));
+    ros::Rate loop_rate(50);
+
+    ros::Time last_time = ros::Time::now();
+    while (ros::ok())
+    {
+        loop_rate.sleep();
+
+        ros::Time current_time = ros::Time::now();
+        ros::Duration elapsed_time = current_time - last_time;
+        last_time = current_time;
+
+        robotnik_base_hw_lib->update();
+        
+        robotnik_base_hw_lib->read(elapsed_time);
+
+        cm.update(current_time, elapsed_time);
+
+        robotnik_base_hw_lib->write(elapsed_time);
+    }
+
+    // Wait until shutdown signal recieved
+    ros::waitForShutdown();
+
+    return 0;
+}
